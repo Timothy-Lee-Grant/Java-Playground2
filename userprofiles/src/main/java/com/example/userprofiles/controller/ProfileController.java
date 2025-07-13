b1e5ebf6a0493da04f993667de9e6140c2131503
Timothy Grant <timothyleegrant1995@gmail.com>
2025-07-13 12:55:14 -0700

    Attempted to add the microservice functionality, but as of right now, my userprofiles microservice is not working.

---
+++ userprofiles/src/main/java/com/example/userprofiles/controller/ProfileController.java
@@ -0,0 +1,19 @@
+package main.java.com.example.userprofiles.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+
+
+
+@RestController
+@RequestMapping("/profiles")
+public class ProfileController {
+    @GetMapping("/{username}")
+    public ResponseEntity<String> getProfile(@PathVariable String username) {
+        System.out.println("you are here");
+        return ResponseEntity.ok("Profile info for " + username);
+    }
+}
