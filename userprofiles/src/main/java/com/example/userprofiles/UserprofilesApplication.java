cb2dca8b788cdd93be22ffb8b5323cf4eef2db7f
Timothy Grant <timothyleegrant1995@gmail.com>
2025-07-12 21:33:09 -0700

    Moved monoligh files into userprofiles microservice.

--- userprofiles/src/main/java/com/example/userprofiles/UserprofilesApplication.java
+++ userprofiles/src/main/java/com/example/userprofiles/UserprofilesApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.userprofiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class UserprofilesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(UserprofilesApplication.class, args);
 	}
 
 }
--- userprofiles/src/test/java/com/example/userprofiles/UserprofilesApplicationTests.java
+++ userprofiles/src/test/java/com/example/userprofiles/UserprofilesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.userprofiles;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class UserprofilesApplicationTests {
 
 	@Test
 	void contextLoads() {
