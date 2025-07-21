cb8e63d6ca06b120df6bbc624e8e10ec87ca7d49
Timothy Grant <timothyleegrant1995@gmail.com>
2025-07-20 23:22:12 -0700

    Pushing my code to github so I can get my little green dot for the day.

--- livedata/src/main/java/com/example/liveupdates/livedata/controller/datafeed.java
+++ livedata/src/main/java/com/example/liveupdates/livedata/controller/datafeed.java
@@ -1,5 +1,5 @@
 package main.java.com.example.liveupdates.livedata.controller;
 
 public class datafeed {
-    
+     
 }
--- server-application/src/main/java/com/example/demo/controller/GreetingController.java
+++ server-application/src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,8 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Greeting;
+//import com.example.demo.model.Greeting;
 import com.example.demo.service.GreetingService;
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
